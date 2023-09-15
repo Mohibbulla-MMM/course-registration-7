4cccf68bfb5cc26422d3da24aa3e6d8cc3cc2763
Mohibbulla-mmm <rockychain1020@gmail.com>
2023-09-15 09:15:12 +0600

    simple header create

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header >
+    <div className="flex sm:flex-row  flex-col gap-4 justify-between items-center py-4 shadow-md  px-3">
+    <h1 className=" md:text-3xl text-2xl font-semibold  ">Course Registration</h1>
+      <figure>
+        <img
+          className="w-16 rounded-full inline-block cursor-pointer"
+          src={"https://i.ibb.co/0mZJptP/boy2.jpg"}
+          alt=" site logo"
+        />
+      </figure>
+    </div>
+    </header>
+  );
+};
+
+export default Header;
